f1700f4f347a8635e9c3f2a8e653926ab6f7653f
Christoffer Petersen <chrisp.dev@outlook.com>
2021-02-19 11:07:20 +0100

    Galleri liste visning lavet med Bindings

--- ValgfagsApp/ValgfagsApp/GalleriSide.xaml.cs
+++ ValgfagsApp/ValgfagsApp/GalleriSide.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,21 @@ namespace ValgfagsApp
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class GalleriSide : ContentPage
     {
+        readonly ObservableCollection<GalleryBindings> gallery = new ObservableCollection<GalleryBindings>();
+        public ObservableCollection<GalleryBindings> Gallery { get { return gallery; } }
+
         public GalleriSide()
         {
             InitializeComponent();
+
+            GalleryView.ItemsSource = gallery;
+
+            gallery.Add(new GalleryBindings { Image = "Christoffer.jpg", Title = "Profil billede", Description = "Dette er mit profil billede, er jeg ikke flot." });
+            gallery.Add(new GalleryBindings { Image = "RedRingHood.jpeg", Title = "Standard baggrunden", Description = "Bruger dette billede som baggrund på alt jeg kan sætte det på." });
+            gallery.Add(new GalleryBindings { Image = "Christoffer.jpg", Title = "Profil billede", Description = "Dette er mit profil billede, er jeg ikke flot." });
+            gallery.Add(new GalleryBindings { Image = "RedRingHood.jpeg", Title = "Standard baggrunden", Description = "Bruger dette billede som baggrund på alt jeg kan sætte det på." });
+            gallery.Add(new GalleryBindings { Image = "Christoffer.jpg", Title = "Profil billede", Description = "Dette er mit profil billede, er jeg ikke flot." });
+            gallery.Add(new GalleryBindings { Image = "RedRingHood.jpeg", Title = "Standard baggrunden", Description = "Bruger dette billede som baggrund på alt jeg kan sætte det på." });
         }
         private async void ProfilNav_Clicked(object sender, System.EventArgs e)
         {
@@ -29,6 +42,11 @@ namespace ValgfagsApp
         private async void GalleriNav_Clicked(object sender, System.EventArgs e)
         {
             await Navigation.PushAsync(new GalleriSide());
+        }
+
+        private void GetImgFromDeviceBtn_Clicked(object sender, EventArgs e)
+        {
+
         }
     }
 }
