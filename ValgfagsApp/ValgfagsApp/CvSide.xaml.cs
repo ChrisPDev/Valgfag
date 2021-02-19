dfb6f48f22d88c7573ce53d8f056a8230aad336f
Christoffer Petersen <chrisp.dev@outlook.com>
2021-02-19 10:12:35 +0100

    CV side persistence færdig

--- ValgfagsApp/ValgfagsApp/CvSide.xaml.cs
+++ ValgfagsApp/ValgfagsApp/CvSide.xaml.cs
@@ -15,6 +15,24 @@ namespace ValgfagsApp
         public CvSide()
         {
             InitializeComponent();
+
+            if (Application.Current.Properties.ContainsKey("UddannelseText")) UddannelseText.Text = Application.Current.Properties["UddannelseText"].ToString();
+            if (Application.Current.Properties.ContainsKey("TidligereJobsText")) TidligereJobsText.Text = Application.Current.Properties["TidligereJobsText"].ToString();
+            if (Application.Current.Properties.ContainsKey("StyrkerText")) StyrkerText.Text = Application.Current.Properties["StyrkerText"].ToString();
+            if (Application.Current.Properties.ContainsKey("InteresserText")) InteresserText.Text = Application.Current.Properties["InteresserText"].ToString();
+            if (Application.Current.Properties.ContainsKey("GenereltText")) GenereltText.Text = Application.Current.Properties["GenereltText"].ToString();
+        }
+        private void OnChange(object sender, EventArgs e)
+        {
+            Application.Current.Properties["UddannelseText"] = UddannelseText.Text;
+            Application.Current.Properties["TidligereJobsText"] = TidligereJobsText.Text;
+            Application.Current.Properties["StyrkerText"] = StyrkerText.Text;
+            Application.Current.Properties["InteresserText"] = InteresserText.Text;
+            Application.Current.Properties["GenereltText"] = GenereltText.Text;
+        }
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
         }
         private async void ProfilNav_Clicked(object sender, System.EventArgs e)
         {
